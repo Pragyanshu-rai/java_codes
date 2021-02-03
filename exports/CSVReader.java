827c56d31df0c386eecd260307ffd9cf9fc41223
Pragyanshu <Pragyanshur@gmail.com>
2021-02-03 16:50:02 +0530

    java_codes

--- exports/CSVReader.java
+++ exports/CSVReader.java
@@ -1,24 +1,30 @@
-import org.apache.commons.csv.CSVParser;
-import org.apache.commons.csv.CSVRecord;
+//import org.apache.commons.csv.CSVParser;
+//import org.apache.commons.csv.CSVRecord;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
+//import java.util.ArrayList;
+import java.io.IOException;
+import java.util.Iterator;
 import java.io.FileReader;
+import java.util.HashMap;
 public class CSVReader
 {
     public static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
     public static BufferedReader fileIn;
-    public static CSVParser parser;
-    public static String countryInfo(CSVParser parser, String country)
+    public static HashMap<String, Integer> head = new HashMap<String, Integer>();
+    public static HashMap<Integer, String> rows = new HashMap<Integer, String>();
+    public static Iterator headIt, rowsIt;
+    /*public static String countryInfo(CSVParser parser, String country)
     {
         for(CSVRecord record : parser)
             if(record.contains(country))
-                return record.get("Country") + " : " + record.get("Exports") + " : " + record.get("Value") + "\n";
+                return head("Country") + " : " + head("Exports") + " : " + head("Value") + "\n";
     }
     public static String twoProductsExporter(CSVParser parser, String exportItem1, String exportItem2)
     {
         for(CSVRecord record : parser)
             if(record.contains(exportItem1) && record.contains(EportItem2))
-                return record.get("Country") + "\n";
+                return head("Country") + "\n";
     }
     public static int numberOfExporters(CSVPareser parser, String exportItem)
     {
@@ -27,8 +33,46 @@ public class CSVReader
             if(record.contains(exportItem))
                 count++;
         return count;
+    }*/
+    public static void csvParser(BufferedReader fileIn) throws IOException
+    {
+        String inputArray[],input,inputArrayCopy[];
+
+        inputArray = fileIn.readLine().split(",");
+        inputArrayCopy = inputArray;
+        int index=0,size=0;
+        for(String str : inputArray)
+        {
+            head.put(str,new Integer(index));
+            index++;
+        }
+        index = 0;
+        while((input = fileIn.readLine())!= null)
+        {
+            //inputArray = fileIn.readLine().split(",");
+            rows.put(new Integer(size),input);
+            size++;
+        }
+        while(index < 3)
+        {
+            System.out.print(head.get(inputArray[index])+" ");
+            index++;
+        }
+        System.out.println();
+        index=0;
+        while(index < size)
+        {
+            int test=0;
+            inputArray = rows.get(new Integer(index)).split("\"");
+            inputArrayCopy = inputArray[3].split(",");
+            for(String str : inputArrayCopy)
+                test++;
+            if(test > 4 )
+            System.out.println(inputArray[0]+" "+inputArray[3/*head.get(inputArrayCopy[0])*/]+" ");
+            index++;
+        }
     }
-    public static void main(String []args)
+    public static void main(String []args) throws IOException 
     {
         String fileName;
         System.out.println("Enter the File name");
@@ -37,15 +81,13 @@ public class CSVReader
             fileName = "exports_small.csv";
         fileName = "./" + fileName;
         fileIn = new BufferedReader(new FileReader(fileName));
-        parser = fileIn.getCSVParser();
-        for(CSVRecord record : parser)
-            System.out.print(countryInfo(parser,record.get("Country")));
-        parser = fileIn.getCSVParser();
+        csvParser(fileIn);
+        /*for(CSVRecord record : parser)
+            System.out.print(countryInfo(parser,head("Country")));
         for(CSVRecord record : parser)
             System.out.print(twoProductsExporter(parser, "gold", "diamonds"));
-        parser = fileIn.getCSVParser();
         for(CSVRecord record : parser)
             System.out.print(numberOfExporters(parser, "gold"));
                 
-    }
+    */}
 }
